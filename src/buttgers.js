98db9531a5d722b550a1ee7b7d3245e4ac08f734
Chris Burke <burke1791@gmail.com>
2019-11-27 20:54:07 -0600

    refactored to work on single page apps

--- src/buttgers.js
+++ src/buttgers.js
@@ -1,13 +1,18 @@
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 let regexp = /rutgers/gi;
 let buttgers = 'Buttgers';
 
-let body = document.getElementsByTagName('body');
-let bodyLength = body.length;
+getBodyAndReplace();
 
-if (bodyLength) {
-  for (var i = 0; i < bodyLength; i++) {
-    replaceRutgers(body.item(i));
+function getBodyAndReplace() {
+  let body = document.getElementsByTagName('body');
+  let bodyLength = body.length;
+
+  if (bodyLength) {
+    for (var i = 0; i < bodyLength; i++) {
+      replaceRutgers(body.item(i));
+    }
   }
 }
 
@@ -25,13 +30,21 @@ function replaceRutgers(node) {
   }
 }
 
+var observer = new MutationObserver(function(mutations, observer) {
+  // fired when a mutation occurs
+  getBodyAndReplace();
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
+
 // in case some resources are slow to load, this should catch them
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-    if (bodyLength) {
-      for (var i = 0; i < bodyLength; i++) {
-        replaceRutgers(body.item(i));
-      }
-    }
+    getBodyAndReplace();
   }
 }
