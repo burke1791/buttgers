b1e10ace94094caf8c188b6c6418dc25ef4e1fbd
Chris Burke <burke1791@gmail.com>
2019-12-11 19:52:35 -0600

    added a limit to how often it tries to replace rutgers in response to DOM mutations, and put the DOM replace on an interval

--- src/buttgers.js
+++ src/buttgers.js
@@ -3,20 +3,35 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 let regexp = /rutgers/gi;
 let buttgers = 'Buttgers';
 
+let mutationMetrics = {
+  prev: new Date(),
+  count: 0
+}
+
+let mutationFlag = true;
+
 getBodyAndReplace();
 
-function getBodyAndReplace() {
-  let body = document.getElementsByTagName('body');
-  let bodyLength = body.length;
+setInterval(getBodyAndReplace, 5000);
 
-  if (bodyLength) {
-    for (var i = 0; i < bodyLength; i++) {
-      replaceRutgers(body.item(i));
+function getBodyAndReplace() {
+  if (mutationFlag) {
+    let body = document.getElementsByTagName('body');
+    let bodyLength = body.length;
+
+    if (bodyLength) {
+      for (var i = 0; i < bodyLength; i++) {
+        replaceRutgers(body.item(i));
+      }
     }
+
+    mutationFlag = false;
   }
 }
 
 function replaceRutgers(node) {
+  updateMutationCount();
+
   if (!node.hasChildNodes()) {
     if (node.nodeType == 3) {
       node.textContent = node.textContent.replace(regexp, buttgers);
@@ -30,14 +45,45 @@ function replaceRutgers(node) {
   }
 }
 
-var observer = new MutationObserver(function(mutations, observer) {
+function updateMutationMetrics() {
+  let now = new Date();
+
+  if (now - mutationMetrics.prev >= 1000) {
+    resetMutationCount();
+  } else {
+    updateMutationCount()
+  }
+}
+
+function updateMutationCount() {
+  let newCount = mutationMetrics.count++;
+  mutationMetrics.count = newCount;
+}
+
+function resetMutationCount(timestamp) {
+  console.log('mutations reset');
+  mutationMetrics.prev = timestamp;
+  mutationMetrics.count = 0;
+}
+
+
+var observer = new MutationObserver(function(mutations) {
   // fired when a mutation occurs
-  getBodyAndReplace();
+  mutations.forEach(mutation => {
+    updateMutationMetrics();
+
+    if (mutationMetrics.count < 5) {
+      replaceRutgers(mutation.target);
+      // getBodyAndReplace();
+    } else {
+      console.log('mutation limit reached');
+    }
+  });
 });
 
 // define what element should be observed by the observer
 // and what types of mutations trigger the callback
-observer.observe(document, {
+observer.observe(document.body, {
   subtree: true,
   attributes: true
 });
